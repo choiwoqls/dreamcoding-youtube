0056c03f0f347814ecb0ddc0a8aec90c4bd9233e
chlwoqls <byin0209@naver.com>
2023-11-16 22:27:21 +0900

    1-6 강의 완료

---
+++ 1-6/object.js
@@ -0,0 +1,72 @@
+//Object
+//변수 하나 당 값 하나
+
+const obj1 = {}; //Object literal
+const obj2 = new Object(); //Object constructor
+
+const name = "jb";
+const age = 24;
+
+print(name, age);
+
+function print(person) {
+    console.log(person.name);
+    console.log(person.age);
+}
+
+const jb = { name: "jb", age: 24 };
+
+print(jb);
+
+jb.hasJob = true;
+
+delete jb.hasJob;
+
+console.log(jb.name);
+console.log(jb["name"]);
+jb["hasJob"] = true;
+
+function printValue(obj, key) {
+    console.log(obj[key]);
+}
+
+function makePerson(name, age) {
+    //template
+    return {
+        name: name, //key == value 이면
+        age, //생략 가능
+    };
+}
+
+//in operator
+console.log("name" in jb);
+console.log("age" in jb);
+console.log("random" in jb);
+
+//for in, for of
+//for (key in obj)
+for (key in jb) {
+    console.log(key);
+}
+//for (value of iterable)
+const array = [1, 2, 4, 5];
+
+for (value of array) {
+    console.log(value);
+}
+
+const user = { name: "jj", age: 20 };
+const user2 = user;
+
+const user4 = {};
+
+//복사
+Object.assign(user4, user);
+
+const user5 = Object.assign({}, user);
+
+const fruit1 = { color: "red" };
+const fruit2 = { color: "blue", size: "big" };
+const mixed = Object.assign({}, fruit1, fruit2); //뒤에 있는 것을 덮어 씌움
+//mixed.color = blue;
+//mixed.size = big;
